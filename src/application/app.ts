bd7090411ad94121b250a79cc3bcc22b1b66913d
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2020-10-31 14:29:52 +0100

    update: movement and garden

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js';
 import * as ECS from '@libs/pixi-ecs';
+import * as _ from 'lodash';
 
 import floor01 from '../assets/floor01.png';
 import floor02 from '../assets/floor02.png';
@@ -10,38 +11,45 @@ import sample01 from '../assets/sample01.png';
 import sample02 from '../assets/sample02.png';
 import doctor01 from '../assets/doctor01.png';
 import grass01 from '../assets/grass01.png';
+import bush01 from '../assets/bush01.png';
 import mazes from './maze.json5';
 
 PIXI.settings.ROUND_PIXELS = true;
 
 const FLAG_MOVING = 1;
 
-class MovingComponent extends ECS.Component {
+class MovingEventComponent extends ECS.Component {
 	onUpdate(delta: number, absolute: number) {
 		let cmp = this.scene.findGlobalComponentByName<ECS.KeyInputComponent>(
 			ECS.KeyInputComponent.name,
 		);
 
 		const distance = delta * 0.2;
+		let coodrsShift = [0, 0];
+		// const distance = Math.ceil(delta * 0.2);
 
 		if (cmp.isKeyPressed(ECS.Keys.KEY_D) && this._hasCollisionMaze(this.owner, distance, 0)) {
-			this.sendMessage('MOVE_IT', [distance, 0]);
-			this.owner.position.x += distance;
+			const [x, y] = coodrsShift;
+			coodrsShift = [x + distance, y];
 		}
 
 		if (cmp.isKeyPressed(ECS.Keys.KEY_A) && this._hasCollisionMaze(this.owner, -distance, 0)) {
-			this.sendMessage('MOVE_IT', [-distance, 0]);
-			this.owner.position.x += -distance;
+			const [x, y] = coodrsShift;
+			coodrsShift = [x - distance, y];
 		}
 
 		if (cmp.isKeyPressed(ECS.Keys.KEY_W) && this._hasCollisionMaze(this.owner, 0, -distance)) {
-			this.sendMessage('MOVE_IT', [0, -distance]);
-			this.owner.position.y += -distance;
+			const [x, y] = coodrsShift;
+			coodrsShift = [x, y - distance];
 		}
 
 		if (cmp.isKeyPressed(ECS.Keys.KEY_S) && this._hasCollisionMaze(this.owner, 0, distance)) {
-			this.sendMessage('MOVE_IT', [0, distance]);
-			this.owner.position.y += distance;
+			const [x, y] = coodrsShift;
+			coodrsShift = [x, y + distance];
+		}
+
+		if (!_.isEqual(coodrsShift, [0, 0])) {
+			this.sendMessage('MOVE_IT', coodrsShift);
 		}
 	}
 
@@ -64,29 +72,57 @@ class MovingComponent extends ECS.Component {
 }
 
 class MovingReverseComponent extends ECS.Component {
-	state = [];
+	state = [0, 0];
+
+	private _setState(coords = [0, 0]) {
+		this.state = coords;
+	}
 
 	onInit() {
 		this.subscribe('MOVE_IT');
 	}
 
 	onMessage(msg: ECS.Message): any {
-		if (msg.action === 'MOVE_IT') this.owner.setFlag(FLAG_MOVING);
+		if (msg.action === 'MOVE_IT') {
+			const [x, y] = this.state;
+			const [xS, yS] = msg.data;
+			this._setState([x + xS, y + yS]);
+		}
 	}
 
 	onUpdate(delta: number, absolute: number) {
-		if (this.owner.hasFlag(FLAG_MOVING)) {
-			let cmp = this.scene.findGlobalComponentByName<ECS.KeyInputComponent>(
-				ECS.KeyInputComponent.name,
-			);
+		const { x, y } = this.owner.position;
+		const [xS, yS] = this.state;
+		this.owner.position.set(x - xS, y - yS);
+		this._setState();
+		// }
+	}
+}
 
-			if (cmp.isKeyPressed(ECS.Keys.KEY_D)) this.owner.position.x += delta * -0.2;
-			if (cmp.isKeyPressed(ECS.Keys.KEY_A)) this.owner.position.x += delta * 0.2;
-			if (cmp.isKeyPressed(ECS.Keys.KEY_W)) this.owner.position.y += delta * 0.2;
-			if (cmp.isKeyPressed(ECS.Keys.KEY_S)) this.owner.position.y += delta * -0.2;
+class MovingComponent extends ECS.Component {
+	state = [0, 0];
 
-			this.owner.resetFlag(FLAG_MOVING);
+	private _setState(coords = [0, 0]) {
+		this.state = coords;
+	}
+
+	onInit() {
+		this.subscribe('MOVE_IT');
+	}
+
+	onMessage(msg: ECS.Message): any {
+		if (msg.action === 'MOVE_IT') {
+			const [x, y] = this.state;
+			const [xS, yS] = msg.data;
+			this._setState([x + xS, y + yS]);
 		}
+	}
+
+	onUpdate(delta: number, absolute: number) {
+		const { x, y } = this.owner.position;
+		const [xS, yS] = this.state;
+		this.owner.position.set(x + xS, y + yS);
+		this._setState();
 	}
 }
 
@@ -102,6 +138,7 @@ class App {
 			height: window.innerHeight,
 			antialias: false,
 			resolution: 1,
+			// resolution: 2,
 		});
 
 		const mazeMatrix = mazes.mapsHexa.alpha;
@@ -116,8 +153,6 @@ class App {
 
 		const containerGarden = new ECS.Container();
 		containerGarden.pivot.set(containerBigWidth / 2, containerBigHeight / 2);
-		containerGarden.width = containerBigWidth;
-		containerGarden.height = containerBigHeight;
 		containerGarden.position.set(canvas.width / 2, canvas.height / 2);
 
 		const containerSmallWidth = mazeWidthBlocks * mazeBlockSize;
@@ -167,6 +202,21 @@ class App {
 			containerBigWidth,
 			containerBigHeight,
 		);
+		garden.zIndex = 1;
+		containerGarden.addChild(garden);
+
+		for (let r = 0; r < containerBigWidth; r += 80) {
+			for (let c = 0; c < containerBigHeight; c += 80) {
+				const rnd = Math.random();
+				if (rnd > 0) {
+					let bush = loader.resources.bush01;
+					const rectangle = new PIXI.Sprite(bush.texture);
+					rectangle.position.set(c + rnd * 100, r + rnd * 100);
+					rectangle.zIndex = 2;
+					containerGarden.addChild(rectangle);
+				}
+			}
+		}
 
 		const doctor = new ECS.Sprite('doctor', loader.resources.doctor01.texture);
 		doctor.height = 24;
@@ -175,13 +225,13 @@ class App {
 		doctor.position.set(containerSmallWidthShift, containerSmallHeightShift);
 
 		containerGarden.addComponent(new MovingReverseComponent());
+		doctor.addComponent(new MovingEventComponent());
 		doctor.addComponent(new MovingComponent());
 		// containerMaze.addComponent(new MovingReverseComponent());
 
 		// Graphics
 
 		this.engine.scene.stage.addChild(containerGarden);
-		containerGarden.addChild(garden);
 		containerGarden.addChild(containerMaze);
 		containerMaze.addChild(doctor);
 
@@ -207,6 +257,7 @@ const loadResource = list =>
 		{ name: 'sample02', url: sample02 },
 		{ name: 'doctor01', url: doctor01 },
 		{ name: 'grass01', url: grass01 },
+		{ name: 'bush01', url: bush01 },
 	]);
 
 	new App();
